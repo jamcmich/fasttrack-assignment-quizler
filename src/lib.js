d431f593b361009d1bd323edac1702ddc567011b
Jacob McMichael <jacobmcmichael@gmail.com>
2022-01-06 12:43:38 -0800

    Revised logic for all methods. Working on failed tests for createPrompt() and createQuestions().

--- src/lib.js
+++ src/lib.js
@@ -3,12 +3,12 @@ import { prompts } from 'inquirer';
 
 /**
  * 
- * @param {*} questions 
- * @param {*} rand 
+ * @param {*} arr 
+ * @param {*} numItems 
  * 
  * [ x ] Implement 'chooseRandom' function
  * [ x ] Should take 2 parameters, 'array' and 'number'
- * [ ] 'array' should default to an empty array
+ * [ x ] 'array' should default to an empty array
  * [ x ] If 'array' has length 0 or 1, then simply return it
  * [ x ] 'numItems' must be checked to ensure it is a number in the range 1 to 'array.length' (inclusive)
  * [ x ] If 'numItems' is outside of the correct range, then it should be set to a random number within the correct range
@@ -19,71 +19,124 @@ import { prompts } from 'inquirer';
  * [ ] Run command 'npm test' and ensure all tests for chooseRandom pass successfully
  * 
  */
-export const chooseRandom = (questions = [], numItems) => {
-  console.log("");
-  console.log("Questions: ", questions, " Items: ", numItems);
+export const chooseRandom = (arr = [], numItems) => {
+  // console.log("");
+  // console.log("Questions: ", arr, " Items: ", numItems);
 
   // base cases
-  if (questions.length <= 1) return questions;
-  if (numItems < 1 || numItems >= questions.length || numItems === undefined) numItems = Math.floor(Math.random() * (questions.length) + 1);
-  console.log("Min: ", 1, "Max: ", questions.length);
-  console.log("Re-Items: ", numItems);
+  if (numItems < 1 || numItems > arr.length || numItems === undefined) {
+    numItems = Math.floor(Math.random() * (arr.length) + 1);
+    // console.log("Re-Items: ", numItems);
+  }
+  // console.log("Min: ", 1, "Max: ", arr.length);
+  if (arr.length <= 1) return arr;
 
   // non-mutated array assignment
-  let shuffled = [...questions];
+  let questions = [...arr];
 
   // Fisher Yates method for random sorting
-  for (let i = shuffled.length - 1; i > 0; i--) {
+  for (let i = questions.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * i)
-    let k = shuffled[i]
-    shuffled[i] = shuffled[j]
-    shuffled[j] = k
+    let k = questions[i]
+    questions[i] = questions[j]
+    questions[j] = k
   }
-  console.log("Shuffled: ", shuffled);
-  console.log("Result: ", shuffled.slice(0, numItems));
+  // console.log("Sorted Questions: ", questions);
+  // console.log("Result: ", questions.slice(0, numItems));
 
   // return array of specified length
-  return shuffled.slice(0, numItems);
+  return questions.slice(0, numItems);
 }
 
 /**
  * 
- * @param {*} questions 
- * @param {*} numItems 
+ * @param {*} obj 
  * 
- * [ ] Implement createPrompt using the given format below and the given test cases
- * [ ] Implement the createQuestions function based on the format below and provided tests
- * [ ] Ensure all tests for createPrompt & createQuestions are passing when you run the npm test command
+ * [ ] Implement 'createPrompt' using the given format below and the given test cases
+ * [ ] Ensure all tests for 'createPrompt' & 'createQuestions' are passing when you run the npm test command
  * 
  */
 export const createPrompt = (obj = { numQuestions: 1, numChoices: 2 }) => {
-  let questions = [];
+  let questions = { ...obj };
+  let result = [];
 
-  for (let q in obj["numQuestions"]) {
-    questions.push(
+  // console.log("ORIGINAL_OBJECT: ", obj);
+  // console.log("ORIGINAL_QUESTIONS: ", questions);
+
+  // base cases
+  if (obj["numQuestions"] < 1) questions["numQuestions"] = 1;
+  if (obj["numChoices"] < 2) questions["numChoices"] = 2;
+  if (obj["numQuestions"] === undefined || obj["numChoices"] < 2 === undefined) {
+    questions = {
+      numQuestions: 1,
+      numChoices: 2
+    }
+  }
+  // console.log('CHANGES', questions);
+
+  for (let q = 1; q <= questions["numQuestions"]; q++) {
+    result.push(
       {
         type: 'input',
-        name: `question-${obj["numQuestions"][q]}`,
-        message: `Enter question ${obj["numQuestions"][q]}`
+        name: `question-${q}`,
+        message: `Enter question ${q}`
       }
     );
 
-    for (let c in obj["numChoices"]) {
-      obj.push(
+    for (let c = 1; c <= questions["numChoices"]; c++) {
+      result.push(
         {
           type: 'input',
-          name: `question-${obj["numQuestions"][q]}-choice-${obj["numChoices"][c]}`,
-          message: `Enter answer choice ${obj["numChoices"][c]} 1 for question ${obj["numQuestions"][q]}`
+          name: `question-${q}-choice-${c}`,
+          message: `Enter answer choice ${c} for question ${q}`
         }
       );
     }
   }
-  console.log(questions);
-  return questions;
+  // console.log("Result: ", result, '\n', result.length);
+  // console.log("ACTUAL: ", result.length);
+  // console.log("TARGET: ", questions["numQuestions"] + (questions["numQuestions"] * questions["numChoices"]));
+  return result;
 }
 
-export const createQuestions = () => {
-  // TODO implement createQuestions
+/**
+ * 
+ * @param {*} obj 
+ * 
+ * [ ] Implement the 'createQuestions' function based on the format below and provided tests
+ * [ ] Ensure all tests for 'createPrompt' & 'createQuestions' are passing when you run the npm test command
+ * 
+ */
+export const createQuestions = (obj = {}) => {
+  /**
+   *      let obj = { 
+   *          question: 'some-text', 
+   *          choice: 'some-choice'
+   *      }
+   */
+
+  console.log('\n', 'INPUT: ', obj);
+
+  let questions = { ...obj };
+  let result = [];
+
+  for (let q in questions) {
+    let choicesArr = [];
+    if (/choice/gi.test(q)) { // if key contains 'choice'
+      choicesArr.push(questions[q]);
+    }
+
+    result.push(
+      {
+        type: 'list',
+        name: q,
+        message: questions[q],
+        choices: choicesArr
+      }
+    )
+  }
+  console.log("OUTPUT: ", result);
+  return result;
 }
 
 export const readFile = path =>
